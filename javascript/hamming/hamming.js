48e3515507d724a340eb8e1674a443e3752ed7d6
Eric Ti Yu Chiang <eric.ti.yu.chiang@gmail.com>
2018-10-12 14:27:01 -0400

    Hamming initial solution

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,5 +1,12 @@
 export default {
-  compute() {
-    return 0;
-  }
+  compute(firstStrand, secondStrand) {
+    if (firstStrand.length !== secondStrand.length) {
+      throw new Error('left and right strands must be of equal length');
+    }
+    let distance = 0;
+    for (let i = 0; i < firstStrand.length; i += 1) {
+      if (firstStrand[i] !== secondStrand[i]) distance += 1;
+    }
+    return distance;
+  },
 };
