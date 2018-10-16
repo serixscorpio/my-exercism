996471605563c97dbed99810c73542dd9a55a708
Eric Ti Yu Chiang <eric.ti.yu.chiang@gmail.com>
2018-10-16 14:01:20 -0400

    bob exercise iteration 1

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,11 +1,14 @@
-/* eslint-disable no-unused-vars */
-//
-// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const hey = message => {
-  //
-  // YOUR CODE GOES HERE
-  //
-}
+export const hey = (rawMessage) => {
+  const questionReg = /\?$/;
+  const yellReg = /^[^a-z]*[A-Z][^a-z]*$/;
+  const message = rawMessage.trim();
+  if (!message) return 'Fine. Be that way!';
+  if (questionReg.test(message)) {
+    if (yellReg.test(message)) return "Calm down, I know what I'm doing!";
+    return 'Sure.';
+  }
+  if (yellReg.test(message)) {
+    return 'Whoa, chill out!';
+  }
+  return 'Whatever.';
+};
