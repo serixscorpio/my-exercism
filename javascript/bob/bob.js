2f2e5831c85d9ad42682f5ad59d3008ab3145fb0
Eric Ti Yu Chiang <eric.ti.yu.chiang@gmail.com>
2018-11-07 17:54:29 -0500

    bob exercise iteration 2
    Based on feedback:
    1. use `endsWith` to check for string ending with `?`
    2. evaluate yellling once and cache the result for later control flow

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,13 +1,12 @@
 export const hey = (rawMessage) => {
-  const questionReg = /\?$/;
-  const yellReg = /^[^a-z]*[A-Z][^a-z]*$/;
   const message = rawMessage.trim();
+  const yelling = /^[^a-z]*[A-Z][^a-z]*$/.test(message);
   if (!message) return 'Fine. Be that way!';
-  if (questionReg.test(message)) {
-    if (yellReg.test(message)) return "Calm down, I know what I'm doing!";
+  if (message.endsWith('?')) {
+    if (yelling) return "Calm down, I know what I'm doing!";
     return 'Sure.';
   }
-  if (yellReg.test(message)) {
+  if (yelling) {
     return 'Whoa, chill out!';
   }
   return 'Whatever.';
