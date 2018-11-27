ec21751afe7d611ab3382f47604930e0f74b281a
Eric Ti Yu Chiang <eric.ti.yu.chiang@gmail.com>
2018-11-27 12:34:48 -0500

    space age iteration 2
    Use object unpacking from function parameters.  This avoids the use of
    `Object.entries`.

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,16 +1,16 @@
 export class SpaceAge {
   constructor(seconds) {
     this.seconds = seconds;
-    Object.entries({
-      Earth: 1,
-      Mercury: 0.2408467,
-      Venus: 0.61519726,
-      Mars: 1.8808158,
-      Jupiter: 11.862615,
-      Saturn: 29.447498,
-      Uranus: 84.016846,
-      Neptune: 164.79132,
-    }).forEach(([planet, ratio]) => {
+    [
+      { planet: 'Earth', ratio: 1 },
+      { planet: 'Mercury', ratio: 0.2408467 },
+      { planet: 'Venus', ratio: 0.61519726 },
+      { planet: 'Mars', ratio: 1.8808158 },
+      { planet: 'Jupiter', ratio: 11.862615 },
+      { planet: 'Saturn', ratio: 29.447498 },
+      { planet: 'Uranus', ratio: 84.016846 },
+      { planet: 'Neptune', ratio: 164.79132 },
+    ].forEach(({planet, ratio}) => {
       this[`on${planet}`] = () => Number((this.seconds / 31557600 / ratio).toFixed(2));
     });
   }
