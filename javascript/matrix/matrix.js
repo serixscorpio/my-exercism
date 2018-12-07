d1cca7c3fec6819bfab2488b5a14bdfe8c4d6c42
Eric Ti Yu Chiang <eric.ti.yu.chiang@gmail.com>
2018-12-07 15:02:45 -0500

    Use `forEach` instead of `for` to shorten expression

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,11 +1,9 @@
 export default class Matrix {
   constructor(inputStr) {
-    this.rows = inputStr.split('\n').map(rowStr => rowStr.split(' ').map(cellStr => parseInt(cellStr, 10)));
-    this.columns = Array.from(this.rows[0], () => []);
-    for (let i = 0; i < this.rows.length; i += 1) {
-      for (let j = 0; j < this.rows[i].length; j += 1) {
-        this.columns[j].push(this.rows[i][j]);
-      }
-    }
+    const rows = inputStr.split('\n').map(rowStr => rowStr.split(' ').map(cellStr => parseInt(cellStr, 10)));
+    const columns = Array.from(rows[0], () => []);
+    rows.forEach(row => row.forEach((number, i) => columns[i].push(number)));
+    this.rows = rows;
+    this.columns = columns;
   }
 }
