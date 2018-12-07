03ae272c92cc63fc6b9a6beb7cc2946f05708ce6
Eric Ti Yu Chiang <eric.ti.yu.chiang@gmail.com>
2018-12-07 10:39:12 -0500

    improve readability based on junedev's feedback

--- javascript/prime-factors/prime-factors.js
+++ javascript/prime-factors/prime-factors.js
@@ -1,7 +1,8 @@
 export const primeFactors = (number) => {
   const result = [];
   let remainingFactor = number;
-  for (let i = 2; remainingFactor !== 1;) {
+  let i = 2;
+  while (remainingFactor > 1) {
     if (remainingFactor % i === 0) {
       result.push(i);
       remainingFactor /= i;
