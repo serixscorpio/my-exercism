866b5b2112969aed87b3619795ae6ecd3b160f78
Eric Ti Yu Chiang <eric.ti.yu.chiang@gmail.com>
2019-01-04 11:28:45 -0500

    Avoid use of `new` in linked-list spec

--- javascript/linked-list/linked-list.spec.js
+++ javascript/linked-list/linked-list.spec.js
@@ -2,35 +2,35 @@ import LinkedList from './linked-list';
 
 describe('LinkedList', () => {
   test('add/extract elements to the end of the list with push/pop', () => {
-    const list = new LinkedList();
+    const list = LinkedList();
     list.push(10);
     list.push(20);
     expect(list.pop()).toBe(20);
     expect(list.pop()).toBe(10);
   });
   xtest('extract elements from the beginning of the list with shift', () => {
-    const list = new LinkedList();
+    const list = LinkedList();
     list.push(10);
     list.push(20);
     expect(list.shift()).toBe(10);
     expect(list.shift()).toBe(20);
   });
   xtest('add/extract elements from the beginning of the list with unshift/shift', () => {
-    const list = new LinkedList();
+    const list = LinkedList();
     list.unshift(10);
     list.unshift(20);
     expect(list.shift()).toBe(20);
     expect(list.shift()).toBe(10);
   });
   xtest('unshift/pop', () => {
-    const list = new LinkedList();
+    const list = LinkedList();
     list.unshift(10);
     list.unshift(20);
     expect(list.pop()).toBe(10);
     expect(list.pop()).toBe(20);
   });
   xtest('example', () => {
-    const list = new LinkedList();
+    const list = LinkedList();
     list.push(10);
     list.push(20);
     expect(list.pop()).toBe(20);
@@ -43,7 +43,7 @@ describe('LinkedList', () => {
     expect(list.shift()).toBe(30);
   });
   xtest('can count its elements', () => {
-    const list = new LinkedList();
+    const list = LinkedList();
     expect(list.count()).toBe(0);
     list.push(10);
     expect(list.count()).toBe(1);
@@ -51,7 +51,7 @@ describe('LinkedList', () => {
     expect(list.count()).toBe(2);
   });
   xtest('sets head/tail after popping last element', () => {
-    const list = new LinkedList();
+    const list = LinkedList();
     list.push(10);
     list.pop();
     list.unshift(20);
@@ -59,7 +59,7 @@ describe('LinkedList', () => {
     expect(list.pop()).toBe(20);
   });
   xtest('sets head/tail after shifting last element', () => {
-    const list = new LinkedList();
+    const list = LinkedList();
     list.unshift(10);
     list.shift();
     list.push(20);
@@ -67,7 +67,7 @@ describe('LinkedList', () => {
     expect(list.shift()).toBe(20);
   });
   xtest('deletes the element with the specified value from the list', () => {
-    const list = new LinkedList();
+    const list = LinkedList();
     list.push(10);
     list.push(20);
     list.push(30);
@@ -77,13 +77,13 @@ describe('LinkedList', () => {
     expect(list.shift()).toBe(10);
   });
   xtest('deletes the only element', () => {
-    const list = new LinkedList();
+    const list = LinkedList();
     list.push(10);
     list.delete(10);
     expect(list.count()).toBe(0);
   });
   xtest('deletes the first of two elements', () => {
-    const list = new LinkedList();
+    const list = LinkedList();
     list.push(10);
     list.push(20);
     list.delete(10);
@@ -91,7 +91,7 @@ describe('LinkedList', () => {
     expect(list.pop()).toBe(20);
   });
   xtest('deletes the second of two elements', () => {
-    const list = new LinkedList();
+    const list = LinkedList();
     list.push(10);
     list.push(20);
     list.delete(20);
@@ -99,7 +99,7 @@ describe('LinkedList', () => {
     expect(list.pop()).toBe(10);
   });
   xtest('delete does not modify the list if the element is not found', () => {
-    const list = new LinkedList();
+    const list = LinkedList();
     list.push(10);
     list.delete(20);
     expect(list.count()).toBe(1);
