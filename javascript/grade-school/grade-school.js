ea106640bca8370b13572198cecfa7d6c99c4606
Eric Ti Yu Chiang <eric.ti.yu.chiang@gmail.com>
2019-02-19 11:48:47 -0500

    grade-school exercise iteration 2
    Address feedback, primarily use:
    1. Ice Factory pattern, see
    https://medium.freecodecamp.org/elegant-patterns-in-modern-javascript-ice-factory-4161859a0eee
    2. Use a reducer instead of JSON parse/stringify
    3. shorten `grade` into one-liner using `Array.slice`

--- javascript/grade-school/grade-school.js
+++ javascript/grade-school/grade-school.js
@@ -1,17 +1,19 @@
 export function GradeSchool() {
   const db = {};
-  return {
-    add(name, gradeNum) {
-      db[gradeNum] = db[gradeNum] || [];
-      db[gradeNum].push(name);
-      db[gradeNum].sort();
-    },
-    grade(gradeNum) {
-      if (!db[gradeNum]) return [];
-      return [...db[gradeNum]];
-    },
-    roster() {
-      return JSON.parse(JSON.stringify(db));
-    },
+  const grade = gradeNum => (db[gradeNum] ? db[gradeNum].slice() : []);
+  const add = (name, gradeNum) => {
+    db[gradeNum] = db[gradeNum] || [];
+    db[gradeNum].push(name);
+    db[gradeNum].sort();
   };
+  const roster = () => Object.keys(db).reduce((accumulator, key) => {
+    accumulator[key] = grade(key);
+    return accumulator;
+  }, {});
+
+  return Object.freeze({
+    add,
+    grade,
+    roster,
+  });
 }
