1e8eceecfba945748b7104fd36b14e67ab85d1f3
Eric Ti Yu Chiang <eric.ti.yu.chiang@gmail.com>
2019-03-20 12:46:48 -0400

    exercise robot-name: improve picking name
    Pre-generate a pool of all possible robot names, and randomly select
    from there.  This ensures we know when all possible names have been
    used.

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -1,27 +1,43 @@
 const nameGen = {
-  randomName() {
-    return `${this.randomLetter()}${this.randomLetter()}${this.randomDigit()}${this.randomDigit()}${this.randomDigit()}`;
-  },
-  randomDigit(max = 10) {
+  randomNumber(max) {
     return Math.floor(Math.random() * Math.floor(max));
-  },
-  randomLetter() {
-    return String.fromCharCode(65 + this.randomDigit(26));
   },
 };
 
+const CHAR_START = 65;
+const CHAR_RANGE = 26;
+const DIGIT_START = 48;
+const DIGIT_RANGE = 10;
+
 const robotNameGen = Object.assign(Object.create(nameGen), {
   init() {
-    this.usedNames = new Set();
-  },
-  nextUniqueName() {
-    while (true) {
-      const name = this.randomName();
-      if (!this.usedNames.has(name)) {
-        this.usedNames.add(name);
-        return name;
+    this.namePool = [];
+    for (let char1 = CHAR_START; char1 < CHAR_START + CHAR_RANGE; char1 += 1) {
+      for (let char2 = CHAR_START; char2 < CHAR_START + CHAR_RANGE; char2 += 1) {
+        for (let digit1 = DIGIT_START; digit1 < DIGIT_START + DIGIT_RANGE; digit1 += 1) {
+          for (let digit2 = DIGIT_START; digit2 < DIGIT_START + DIGIT_RANGE; digit2 += 1) {
+            for (let digit3 = DIGIT_START; digit3 < DIGIT_START + DIGIT_RANGE; digit3 += 1) {
+              this.namePool.push(String.fromCharCode(char1, char2, digit1, digit2, digit3));
+            }
+          }
+        }
       }
     }
+    this.releaseNames();
+  },
+  nextUniqueName() {
+    if (this.numNamesAvailable === 0) {
+      throw new Error('No more name avaiable');
+    }
+    const randomlySelectedIndex = this.randomNumber(this.numNamesAvailable);
+    const lastAvailableIndex = (this.numNamesAvailable -= 1);
+    const pickedName = this.namePool[randomlySelectedIndex];
+    this.namePool[randomlySelectedIndex] = this.namePool[lastAvailableIndex];
+    this.namePool[lastAvailableIndex] = pickedName;
+    return pickedName;
+  },
+  releaseNames() {
+    this.numNamesAvailable = this.namePool.length;
   },
 });
 
@@ -42,8 +58,12 @@ const protoRobot = () => {
   };
 };
 
-export function Robot() {
+export const Robot = () => {
   const robot = Object.create(protoRobot());
   robot.reset();
   return robot;
-}
+};
+
+Robot.releaseNames = () => {
+  robotNameGen.releaseNames();
+};
