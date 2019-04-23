ccf4f0215aa9572ad404a677874abef63e9247b2
Eric Ti Yu Chiang <eric.ti.yu.chiang@gmail.com>
2019-04-23 12:32:26 -0400

    Add `resistor-color.js` content reference
    This is to help illustrate usage of import/export

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,3 +1,11 @@
+// For reference: start of resistor-color.js
+// 
+// export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+//
+// export const colorCode = (color) => COLORS.indexOf(color);
+//
+// end of resistor-color.js
+
 import { colorCode } from '../resistor-color/resistor-color.js';
 
 export const value = ([color1, color2]) => colorCode(color1) * 10 + colorCode(color2) 
