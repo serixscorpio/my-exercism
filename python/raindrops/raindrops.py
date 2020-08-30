cbce3dfecdf57c8cbad6545fd505aa8b8a472dc0
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2020-08-29 21:53:08 -0400

    Python raindrops initial solution

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,11 @@
 def convert(number):
-    pass
+    sound = ""
+    if number % 3 == 0:
+        sound += "Pling"
+    if number % 5 == 0:
+        sound += "Plang"
+    if number % 7 == 0:
+        sound += "Plong"
+    if len(sound) == 0:
+        sound = str(number)
+    return sound
