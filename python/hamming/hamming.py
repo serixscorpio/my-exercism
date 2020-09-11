97468a53c8430aea3d8dc93c56b7174bcd3171f3
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2020-09-11 14:00:39 -0400

    Python hamming initial solution

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,8 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("strands' lengths do not match!")
+    distance = 0
+    for a, b in zip(strand_a, strand_b):
+        if a != b:
+            distance += 1
+    return distance
