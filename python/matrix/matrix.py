59dbcd4e223de6e7b590d859eb4fb79564a35b51
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2020-09-10 21:50:09 -0400

    Modification based on feedback
    1. Use list comprehension where appropriate
    2. Implement `column` in a more readable way.

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -5,13 +5,14 @@ class Matrix:
     def __init__(self, matrix_string):
         self.rows = []
         for row_string in matrix_string.splitlines():
-            row = []
-            for cell_string in row_string.split(" "):
-                row.append(int(cell_string))
+            row = [int(cell_string) for cell_string in row_string.split(" ")]
             self.rows.append(row)
 
     def row(self, index):
-        return self.rows[index-1]
+        return self.rows[index - 1]
 
     def column(self, index):
-        return list(nth(zip(*self.rows), index-1))
+        col = []
+        for i in range(len(self.rows)):
+            col.append(self.rows[i][index - 1])
+        return col
