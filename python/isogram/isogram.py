e928500e748890201439aab0323217e561e9a8eb
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2020-09-20 13:55:07 -0400

    WIP: initial implementation for isogram (Python)

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,8 @@
-def is_isogram(string):
-    pass
+from collections import defaultdict
+import string
+
+
+def is_isogram(input_string):
+    letters_seen_before = defaultdict(bool)
+    for character in input_string.lower():
+        pass
