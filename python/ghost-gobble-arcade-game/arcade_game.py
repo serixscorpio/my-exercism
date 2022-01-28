33e8910aec003d747c8108f760f5af0cdd173d64
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2022-01-28 12:05:17 -0500

    Implement rules

--- python/ghost-gobble-arcade-game/arcade_game.py
+++ python/ghost-gobble-arcade-game/arcade_game.py
@@ -11,27 +11,29 @@ def eat_ghost(power_pellet_active: bool, touching_ghost: bool) -> bool:
     return power_pellet_active and touching_ghost
 
 
-def score(touching_power_pellet, touching_dot):
+def score(touching_power_pellet: bool, touching_dot: bool) -> bool:
     """
 
     :param touching_power_pellet: bool - does the player have an active power pellet?
     :param touching_dot:  bool - is the player touching a dot?
     :return: bool
     """
-    pass
+    return touching_power_pellet or touching_dot
 
 
-def lose(power_pellet_active, touching_ghost):
+def lose(power_pellet_active: bool, touching_ghost: bool) -> bool:
     """
 
     :param power_pellet_active: bool - does the player have an active power pellet?
     :param touching_ghost: bool - is the player touching a ghost?
     :return: bool
     """
-    pass
+    return not power_pellet_active and touching_ghost
 
 
-def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
+def win(
+    has_eaten_all_dots: bool, power_pellet_active: bool, touching_ghost: bool
+) -> bool:
     """
 
     :param has_eaten_all_dots: bool - has the player "eaten" all the dots?
@@ -39,4 +41,4 @@ def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
     :param touching_ghost:  bool - is the player touching a ghost?
     :return: bool
     """
-    pass
+    return has_eaten_all_dots and not lose(power_pellet_active, touching_ghost)
