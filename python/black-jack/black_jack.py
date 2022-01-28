ff279dceac482d9a2b07e6cd173cc8080f929d74
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2022-01-28 16:03:49 -0500

    Avoid using match..case syntax
    The ci tooling doesn't support python 3.10 yet, so avoid using
    match..case syntax, which is only available in python 3.10.

--- python/black-jack/black_jack.py
+++ python/black-jack/black_jack.py
@@ -14,13 +14,11 @@ def value_of_card(card: str) -> int:
     :param card: str - given card.
     :return: int - value of a given card. 'J', 'Q', 'K' = 10; 'A' = 1; numerical value otherwise.
     """
-    match card:
-        case "J" | "Q" | "K":
-            return 10
-        case "A":
-            return 1
-        case _:
-            return int(card)
+    if card == "A":
+        return 1
+    if card == "J" or card == "Q" or card == "K":
+        return 10
+    return int(card)
 
 
 def higher_card(card_one: str, card_two: str) -> Union[str, tuple[str, str]]:
@@ -62,14 +60,14 @@ def is_blackjack(card_one: str, card_two: str) -> bool:
     numerical value otherwise.
     :return: bool - if the hand is a blackjack (two cards worth 21).
     """
-    match (value_of_card(card_one), value_of_card(card_two)):
-        case (1, 10) | (10, 1):
-            return True
-        case _:
-            return False
+    value_one: int = value_of_card(card_one)
+    value_two: int = value_of_card(card_two)
+    if (value_one == 1 and value_two == 10) or (value_one == 10 and value_two == 1):
+        return True
+    return False
 
 
-def can_split_pairs(card_one, card_two):
+def can_split_pairs(card_one: str, card_two: str) -> bool:
     """Determine if a player can split their hand into two hands.
 
     :param card_one, card_two: str - cards dealt.
@@ -80,14 +78,13 @@ def can_split_pairs(card_one, card_two):
     return False
 
 
-def can_double_down(card_one, card_two):
+def can_double_down(card_one: str, card_two: str) -> bool:
     """Determine if a blackjack player can place a double down bet.
 
     :param card_one, card_two: str - first and second cards in hand.
     :return: bool - if the hand can be doubled down (i.e. totals 9, 10 or 11 points).
     """
-    match value_of_card(card_one) + value_of_card(card_two):
-        case 9 | 10 | 11:
-            return True
-        case _:
-            return False
+    two_card_total: int = value_of_card(card_one) + value_of_card(card_two)
+    if two_card_total in (9, 10, 11):
+        return True
+    return False
