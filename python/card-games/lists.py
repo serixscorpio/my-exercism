d84243d34dc623934c65178f182fa27030ba8e2e
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2022-01-28 17:41:52 -0500

    Implement card games

--- python/card-games/lists.py
+++ python/card-games/lists.py
@@ -1,70 +1,71 @@
-def get_rounds(number):
+def get_rounds(number: int) -> list[int]:
     """
 
-     :param number: int - current round number.
-     :return: list - current round and the two that follow.
+    :param number: int - current round number.
+    :return: list - current round and the two that follow.
     """
+    return [number, number + 1, number + 2]
 
-    pass
 
-
-def concatenate_rounds(rounds_1, rounds_2):
+def concatenate_rounds(rounds_1: list, rounds_2: list) -> list:
     """
 
     :param rounds_1: list - first rounds played.
     :param rounds_2: list - second set of rounds played.
     :return: list - all rounds played.
     """
+    return rounds_1 + rounds_2
 
-    pass
 
-
-def list_contains_round(rounds, number):
+def list_contains_round(rounds: list[int], number: int) -> bool:
     """
 
     :param rounds: list - rounds played.
     :param number: int - round number.
     :return:  bool - was the round played?
     """
+    return number in rounds
 
-    pass
 
-
-def card_average(hand):
+def card_average(hand: list[int]) -> float:
     """
 
     :param hand: list - cards in hand.
     :return:  float - average value of the cards in the hand.
     """
+    return sum(hand) / len(hand)
 
-    pass
 
-
-def approx_average_is_average(hand):
+def approx_average_is_average(hand: list[int]) -> bool:
     """
 
     :param hand: list - cards in hand.
     :return: bool - if approximate average equals to the `true average`.
     """
+    if (hand[0] + hand[-1]) / 2 == card_average(hand):
+        return True
+    if hand[len(hand) // 2] == card_average(hand):  # use floor division
+        return True
+    return False
 
-    pass
 
-
-def average_even_is_average_odd(hand):
+def average_even_is_average_odd(hand: list[int]) -> bool:
     """
 
     :param hand: list - cards in hand.
     :return: bool - are even and odd averages equal?
     """
+    even_positions = hand[0::2]
+    odd_positions = hand[1::2]
+    return card_average(even_positions) == card_average(odd_positions)
 
-    pass
 
-
-def maybe_double_last(hand):
+def maybe_double_last(hand: list[int]) -> list[int]:
     """
 
     :param hand: list - cards in hand.
     :return: list - hand with Jacks (if present) value doubled.
     """
-
-    pass
+    if hand[-1] == 11:
+        hand[-1] *= 2
+    return hand
