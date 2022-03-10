ac36f912137e559b70d31019628043c3b458957b
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2022-03-10 16:41:27 -0500

    Improvements based on bkc (Brad Clements) feedback
    1. Use `isdigit` on a string
    2. Compose conditionals to improve clarity
    3. use @cache decorator to cache luhn algorithm result
    4. Simplify return statement

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,5 +1,6 @@
 """Luhn"""
 import re
+from functools import cache
 
 
 class Luhn:
@@ -8,13 +9,11 @@ class Luhn:
     def __init__(self, card_num: str):
         self.card_num = card_num
 
+    @cache
     def valid(self) -> bool:
         """check if card num is valid using luhn algorithm"""
         card_num_no_space = re.sub(r" ", "", self.card_num)
-        if re.search(r"\D", card_num_no_space):
-            # non-digit found
-            return False
-        if len(card_num_no_space) <= 1:
+        if len(card_num_no_space) <= 1 or not card_num_no_space.isdigit():
             return False
         total = 0
         for index, char in enumerate(reversed(card_num_no_space)):
@@ -26,6 +25,4 @@ class Luhn:
                 if subtotal > 9:
                     subtotal -= 9
                 total += subtotal
-        if total % 10 == 0:
-            return True
-        return False
+        return total % 10 == 0
