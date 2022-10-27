dd505cd7aa9dfdac28d59e69073737eacf5994da
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2022-10-27 14:47:14 +0800

    Typescript two-fer baseline

---
+++ typescript/two-fer/two-fer.test.ts
@@ -0,0 +1,18 @@
+import { twoFer } from './two-fer'
+
+describe('TwoFer', () => {
+  it('no name given', () => {
+    const expected = 'One for you, one for me.'
+    expect(twoFer()).toEqual(expected)
+  })
+
+  it('a name given', () => {
+    const expected = 'One for Alice, one for me.'
+    expect(twoFer({ name: 'Alice' })).toEqual(expected)
+  })
+
+  xit('another name given', () => {
+    const expected = 'One for Bob, one for me.'
+    expect(twoFer({ name: 'Bob' })).toEqual(expected)
+  })
+})
