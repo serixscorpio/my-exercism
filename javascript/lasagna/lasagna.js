5ea7e056faeea4314a3ea285c0254d1a1bc262a5
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2022-10-28 14:36:41 +0800

    JS: lasagna iteration 1

--- javascript/lasagna/lasagna.js
+++ javascript/lasagna/lasagna.js
@@ -36,6 +36,11 @@
 const PREPARATION_MINUTES_PER_LAYER = 2;
 
 /**
+ * The number of minutes a lasagna should be in an oven.
+ */
+export const EXPECTED_MINUTES_IN_OVEN = 40;
+
+/**
  * Determines the number of minutes the lasagna still needs to remain in the
  * oven to be properly prepared.
  *
@@ -43,7 +48,7 @@ const PREPARATION_MINUTES_PER_LAYER = 2;
  * @returns {number} the number of minutes remaining
  */
 export function remainingMinutesInOven(actualMinutesInOven) {
-  throw new Error('Remove this line and implement the function');
+  return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven
 }
 
 /**
@@ -53,7 +58,7 @@ export function remainingMinutesInOven(actualMinutesInOven) {
  * @returns {number} the total preparation time
  */
 export function preparationTimeInMinutes(numberOfLayers) {
-  throw new Error('Remove this line and implement the function');
+  return PREPARATION_MINUTES_PER_LAYER * numberOfLayers
 }
 
 /**
@@ -65,5 +70,5 @@ export function preparationTimeInMinutes(numberOfLayers) {
  * @returns {number} the total working time
  */
 export function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
-  throw new Error('Remove this line and implement the function');
+  return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven
 }
