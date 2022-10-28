307601b607a6cbdd231f20f6fb6378b50f473dfb
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2022-10-28 10:33:05 +0800

    Typescript: resistor initial solution

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,4 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+export function decodedValue(color_codes: string[]): number {
+  return COLORS.indexOf(color_codes[0]) * 10 + COLORS.indexOf(color_codes[1])
 }
