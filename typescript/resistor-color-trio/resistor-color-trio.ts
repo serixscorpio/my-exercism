1dc3dbd53e8b82266d5d770cd2fddd1cdeb8a70d
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2022-10-28 11:16:37 +0800

    TS: resistor trio iteration 1

--- typescript/resistor-color-trio/resistor-color-trio.ts
+++ typescript/resistor-color-trio/resistor-color-trio.ts
@@ -1,3 +1,9 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this statement and implement this function')
+export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+export function decodedResistorValue([band1, band2, band3]: string[]): string {
+  const ohms = (COLORS.indexOf(band1) * 10 + COLORS.indexOf(band2)) * (10 ** COLORS.indexOf(band3))
+  if (ohms % 1000 == 0) {
+    return `${ohms / 1000} kiloohms`
+  } else {
+    return `${ohms} ohms`
+  }
 }
