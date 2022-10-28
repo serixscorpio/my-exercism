33ebcee63768bd461d849afcd971c435f9ec2d05
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2022-10-28 11:24:50 +0800

    TS: resistor trio iteration 2
    Use ternary expression for return statement, marginally improve
    readability.

--- typescript/resistor-color-trio/resistor-color-trio.ts
+++ typescript/resistor-color-trio/resistor-color-trio.ts
@@ -1,9 +1,5 @@
 export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
 export function decodedResistorValue([band1, band2, band3]: string[]): string {
   const ohms = (COLORS.indexOf(band1) * 10 + COLORS.indexOf(band2)) * (10 ** COLORS.indexOf(band3))
-  if (ohms % 1000 == 0) {
-    return `${ohms / 1000} kiloohms`
-  } else {
-    return `${ohms} ohms`
-  }
+  return (ohms % 1000 == 0) ? `${ohms / 1000} kiloohms` : `${ohms} ohms`
 }
