dbdd37c91148c9563d02b2d85bea2282206e5508
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2022-10-28 10:51:52 +0800

    TS: resistor iteration 2

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,4 +1,4 @@
 export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
-export function decodedValue(color_codes: string[]): number {
-  return COLORS.indexOf(color_codes[0]) * 10 + COLORS.indexOf(color_codes[1])
+export function decodedValue([band1, band2]: string[]): number {
+  return COLORS.indexOf(band1) * 10 + COLORS.indexOf(band2)
 }
