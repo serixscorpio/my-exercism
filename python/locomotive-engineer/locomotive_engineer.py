58b4bb7680be588a997ca68e674235022ad20f6d
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2023-10-28 21:46:19 -0400

    Locomotive Engineer 1st iteration.

--- python/locomotive-engineer/locomotive_engineer.py
+++ python/locomotive-engineer/locomotive_engineer.py
@@ -1,13 +1,13 @@
 """Functions which helps the locomotive engineer to keep track of the train."""
 
 
-def get_list_of_wagons():
+def get_list_of_wagons(*wagons):
     """Return a list of wagons.
 
     :param: arbitrary number of wagons.
     :return: list - list of wagons.
     """
-    pass
+    return list(wagons)
 
 
 def fix_list_of_wagons(each_wagons_id, missing_wagons):
@@ -17,17 +17,18 @@ def fix_list_of_wagons(each_wagons_id, missing_wagons):
     :parm missing_wagons: list - the list of missing wagons.
     :return: list - list of wagons.
     """
-    pass
+    second_last, last, first, *train_1 = each_wagons_id
+    return [first, *missing_wagons, *train_1, second_last, last]
 
 
-def add_missing_stops():
+def add_missing_stops(route: dict, **stops):
     """Add missing stops to route dict.
 
     :param route: dict - the dict of routing information.
     :param: arbitrary number of stops.
     :return: dict - updated route dictionary.
     """
-    pass
+    return {**route, "stops": [*stops.values()]}
 
 
 def extend_route_information(route, more_route_information):
@@ -37,13 +38,13 @@ def extend_route_information(route, more_route_information):
     :param more_route_information: dict -  extra route information.
     :return: dict - extended route information.
     """
-    pass
+    return {**route, **more_route_information}
 
 
-def fix_wagon_depot(wagons_rows):
+def fix_wagon_depot(wagons_rows: list[list[tuple]]):
     """Fix the list of rows of wagons.
 
     :param wagons_rows: list[list[tuple]] - the list of rows of wagons.
     :return: list[list[tuple]] - list of rows of wagons.
     """
-    pass
+    return [list(col) for col in zip(*wagons_rows)]
