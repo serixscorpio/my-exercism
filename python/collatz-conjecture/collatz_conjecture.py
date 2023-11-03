4ad767e99238633a32d6e220e8699e534da77209
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2023-11-02 21:55:14 -0400

    Collatz conjecture initial solution.

---
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -0,0 +1,14 @@
+def steps(number: int) -> int:
+    """Return the number of steps required to reach 1, from the Collatz conjecture"""
+
+    if number < 1:
+        msg = "Only positive integers are allowed"
+        raise ValueError(msg)
+    count = 0
+    while number != 1:
+        if number % 2 == 0:
+            number = number / 2
+        else:
+            number = number * 3 + 1
+        count += 1
+    return count
