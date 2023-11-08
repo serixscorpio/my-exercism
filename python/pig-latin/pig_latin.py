5ff96d730f925479215a0ebfe818b7ec1abcda10
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2023-11-08 10:26:02 -0500

    Python pig latin: add documentation.

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,8 +1,10 @@
 def begins_with_vowel_sound(word: str) -> bool:
+    """Decide if a word starts with a vowel sound."""
     return word[0] in "aeiou" or word[:2] in ["xr", "yt"]
 
 
 def vowel_sound_starts_at(word: str) -> int:
+    """Find a word's vowel sound starting index."""
     if begins_with_vowel_sound(word):
         return 0
     for i in range(1, len(word)):
@@ -14,9 +16,11 @@ def vowel_sound_starts_at(word: str) -> int:
 
 
 def translate_word(word: str) -> str:
+    """Translate a word into pig latin."""
     boundary = vowel_sound_starts_at(word)
     return word[boundary:] + word[:boundary] + "ay"
 
 
 def translate(text: str) -> str:
+    """Translate a text (could be a sentence) into pig latin."""
     return " ".join(map(translate_word, text.split()))
