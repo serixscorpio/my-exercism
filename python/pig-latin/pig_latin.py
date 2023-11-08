46f2a3f8d3013aa546eef92794f8af4baf7ea2c3
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2023-11-08 10:17:01 -0500

    Python pig latin initial solution.

---
+++ python/pig-latin/pig_latin.py
@@ -0,0 +1,22 @@
+def begins_with_vowel_sound(word: str):
+    return word[0] in "aeiou" or word[:2] in ["xr", "yt"]
+
+
+def vowel_sound_starts_at(word: str):
+    if begins_with_vowel_sound(word):
+        return 0
+    for i in range(1, len(word)):
+        if word[i] in "aeiouy":
+            # account for word starts with a consonant sound followed by "qu"
+            if word[i - 1] == "q" and word[i] == "u":
+                return i + 1
+            return i
+
+
+def translate_word(word: str):
+    boundary = vowel_sound_starts_at(word)
+    return word[boundary:] + word[:boundary] + "ay"
+
+
+def translate(text: str):
+    return " ".join(map(translate_word, text.split()))
