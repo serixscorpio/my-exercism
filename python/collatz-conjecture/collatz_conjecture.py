1570ee4d390404644f03f3b446f0d50b8a2f66bb
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2023-12-29 12:22:47 -0500

    Collatz Conjecture: fix typing.

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -7,7 +7,7 @@ def steps(number: int) -> int:
     count = 0
     while number != 1:
         if number % 2 == 0:
-            number = number / 2
+            number = number // 2
         else:
             number = number * 3 + 1
         count += 1
