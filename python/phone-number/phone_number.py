f8b661337d19f20a44b8d21f502308878ebc57d9
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2024-01-07 13:17:02 -0500

    Use inner function to group validation logic.

--- python/phone-number/phone_number.py
+++ python/phone-number/phone_number.py
@@ -11,6 +11,28 @@ class PhoneNumber:
         return f"({self.area_code})-{self.number[3:6]}-{self.number[6:]}"
 
     def _clean_nanp(self, number_str: str) -> str:
+
+        def _adjust_length(number_str: str) -> str:
+            if len(number_str) < 10:
+                raise ValueError("must not be fewer than 10 digits")
+            if len(number_str) > 11:
+                raise ValueError("must not be greater than 11 digits")
+            if len(number_str) == 11:
+                if number_str[0] != "1":
+                    raise ValueError("11 digits must start with 1")
+                return number_str[1:]  # 11 digits, remove leading 1
+            return number_str # 10 digits
+
+        def _check_start_char(number_str: str) -> None:
+            if number_str[0] == "0":
+                raise ValueError("area code cannot start with zero")
+            if number_str[0] == "1":
+                raise ValueError("area code cannot start with one")
+            if number_str[3] == "0":
+                raise ValueError("exchange code cannot start with zero")
+            if number_str[3] == "1":
+                raise ValueError("exchange code cannot start with one")
+
         number_str = re.sub(
             r"[\s.()+-]", "", number_str
         )  # Remove all formatting characters
@@ -18,20 +40,7 @@ class PhoneNumber:
             raise ValueError("letters not permitted")
         if not number_str.isdigit():
             raise ValueError("punctuations not permitted")
-        if len(number_str) < 10:
-            raise ValueError("must not be fewer than 10 digits")
-        if len(number_str) > 11:
-            raise ValueError("must not be greater than 11 digits")
-        if len(number_str) == 11:
-            if number_str[0] != "1":
-                raise ValueError("11 digits must start with 1")
-            number_str = number_str[1:]  # Remove leading 1
-        if number_str[0] == "0":
-            raise ValueError("area code cannot start with zero")
-        if number_str[0] == "1":
-            raise ValueError("area code cannot start with one")
-        if number_str[3] == "0":
-            raise ValueError("exchange code cannot start with zero")
-        if number_str[3] == "1":
-            raise ValueError("exchange code cannot start with one")
+
+        number_str = _adjust_length(number_str)
+        _check_start_char(number_str)
         return number_str
--- python/phone-number/phone_number_test.py
+++ python/phone-number/phone_number_test.py
@@ -10,113 +10,113 @@ from phone_number import (
 
 
 class PhoneNumberTest(unittest.TestCase):
-    def test_cleans_the_number(self):
+    def test_cleans_the_number(self) -> None:
         number = PhoneNumber("(223) 456-7890").number
         self.assertEqual(number, "2234567890")
 
-    def test_cleans_numbers_with_dots(self):
+    def test_cleans_numbers_with_dots(self) -> None:
         number = PhoneNumber("223.456.7890").number
         self.assertEqual(number, "2234567890")
 
-    def test_cleans_numbers_with_multiple_spaces(self):
+    def test_cleans_numbers_with_multiple_spaces(self) -> None:
         number = PhoneNumber("223 456   7890   ").number
         self.assertEqual(number, "2234567890")
 
-    def test_invalid_when_9_digits(self):
+    def test_invalid_when_9_digits(self) -> None:
         with self.assertRaises(ValueError) as err:
             PhoneNumber("123456789")
         self.assertEqual(type(err.exception), ValueError)
         self.assertEqual(err.exception.args[0], "must not be fewer than 10 digits")
 
-    def test_invalid_when_11_digits_does_not_start_with_a_1(self):
+    def test_invalid_when_11_digits_does_not_start_with_a_1(self) -> None:
         with self.assertRaises(ValueError) as err:
             PhoneNumber("22234567890")
         self.assertEqual(type(err.exception), ValueError)
         self.assertEqual(err.exception.args[0], "11 digits must start with 1")
 
-    def test_valid_when_11_digits_and_starting_with_1(self):
+    def test_valid_when_11_digits_and_starting_with_1(self) -> None:
         number = PhoneNumber("12234567890").number
         self.assertEqual(number, "2234567890")
 
-    def test_valid_when_11_digits_and_starting_with_1_even_with_punctuation(self):
+    def test_valid_when_11_digits_and_starting_with_1_even_with_punctuation(self) -> None:
         number = PhoneNumber("+1 (223) 456-7890").number
         self.assertEqual(number, "2234567890")
 
-    def test_invalid_when_more_than_11_digits(self):
+    def test_invalid_when_more_than_11_digits(self) -> None:
         with self.assertRaises(ValueError) as err:
             PhoneNumber("321234567890")
         self.assertEqual(type(err.exception), ValueError)
         self.assertEqual(err.exception.args[0], "must not be greater than 11 digits")
 
-    def test_invalid_with_letters(self):
+    def test_invalid_with_letters(self) -> None:
         with self.assertRaises(ValueError) as err:
             PhoneNumber("523-abc-7890")
         self.assertEqual(type(err.exception), ValueError)
         self.assertEqual(err.exception.args[0], "letters not permitted")
 
-    def test_invalid_with_punctuations(self):
+    def test_invalid_with_punctuations(self) -> None:
         with self.assertRaises(ValueError) as err:
             PhoneNumber("523-@:!-7890")
         self.assertEqual(type(err.exception), ValueError)
         self.assertEqual(err.exception.args[0], "punctuations not permitted")
 
-    def test_invalid_if_area_code_starts_with_0(self):
+    def test_invalid_if_area_code_starts_with_0(self) -> None:
         with self.assertRaises(ValueError) as err:
             PhoneNumber("(023) 456-7890")
         self.assertEqual(type(err.exception), ValueError)
         self.assertEqual(err.exception.args[0], "area code cannot start with zero")
 
-    def test_invalid_if_area_code_starts_with_1(self):
+    def test_invalid_if_area_code_starts_with_1(self) -> None:
         with self.assertRaises(ValueError) as err:
             PhoneNumber("(123) 456-7890")
         self.assertEqual(type(err.exception), ValueError)
         self.assertEqual(err.exception.args[0], "area code cannot start with one")
 
-    def test_invalid_if_exchange_code_starts_with_0(self):
+    def test_invalid_if_exchange_code_starts_with_0(self) -> None:
         with self.assertRaises(ValueError) as err:
             PhoneNumber("(223) 056-7890")
         self.assertEqual(type(err.exception), ValueError)
         self.assertEqual(err.exception.args[0], "exchange code cannot start with zero")
 
-    def test_invalid_if_exchange_code_starts_with_1(self):
+    def test_invalid_if_exchange_code_starts_with_1(self) -> None:
         with self.assertRaises(ValueError) as err:
             PhoneNumber("(223) 156-7890")
         self.assertEqual(type(err.exception), ValueError)
         self.assertEqual(err.exception.args[0], "exchange code cannot start with one")
 
-    def test_invalid_if_area_code_starts_with_0_on_valid_11_digit_number(self):
+    def test_invalid_if_area_code_starts_with_0_on_valid_11_digit_number(self) -> None:
         with self.assertRaises(ValueError) as err:
             PhoneNumber("1 (023) 456-7890")
         self.assertEqual(type(err.exception), ValueError)
         self.assertEqual(err.exception.args[0], "area code cannot start with zero")
 
-    def test_invalid_if_area_code_starts_with_1_on_valid_11_digit_number(self):
+    def test_invalid_if_area_code_starts_with_1_on_valid_11_digit_number(self) -> None:
         with self.assertRaises(ValueError) as err:
             PhoneNumber("1 (123) 456-7890")
         self.assertEqual(type(err.exception), ValueError)
         self.assertEqual(err.exception.args[0], "area code cannot start with one")
 
-    def test_invalid_if_exchange_code_starts_with_0_on_valid_11_digit_number(self):
+    def test_invalid_if_exchange_code_starts_with_0_on_valid_11_digit_number(self) -> None:
         with self.assertRaises(ValueError) as err:
             PhoneNumber("1 (223) 056-7890")
         self.assertEqual(type(err.exception), ValueError)
         self.assertEqual(err.exception.args[0], "exchange code cannot start with zero")
 
-    def test_invalid_if_exchange_code_starts_with_1_on_valid_11_digit_number(self):
+    def test_invalid_if_exchange_code_starts_with_1_on_valid_11_digit_number(self) -> None:
         with self.assertRaises(ValueError) as err:
             PhoneNumber("1 (223) 156-7890")
         self.assertEqual(type(err.exception), ValueError)
         self.assertEqual(err.exception.args[0], "exchange code cannot start with one")
 
     # Additional tests for this track
-    def test_area_code(self):
+    def test_area_code(self) -> None:
         number = PhoneNumber("2234567890")
         self.assertEqual(number.area_code, "223")
 
-    def test_pretty_print(self):
+    def test_pretty_print(self) -> None:
         number = PhoneNumber("2234567890")
         self.assertEqual(number.pretty(), "(223)-456-7890")
 
-    def test_pretty_print_with_full_us_phone_number(self):
+    def test_pretty_print_with_full_us_phone_number(self) -> None:
         number = PhoneNumber("12234567890")
         self.assertEqual(number.pretty(), "(223)-456-7890")
